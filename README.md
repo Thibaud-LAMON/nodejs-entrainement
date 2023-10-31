c04925724eb79af60293a38abd52eb3df607740e
Thibaud-LAMON <thib.lamon@gmail.com>
2023-10-31 16:33:18 +0100

    Préparez la BDD pour les infos d'authentification

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # nodejs-entrainement
 
-Création d'un backend en nodejs et MongoDB en suivant le cours : https://openclassrooms.com/fr/courses/6390246-passez-au-full-stack-avec-node-js-express-et-mongodb
+Création d'un backend en nodejs et MongoDB en suivant le cours suivant : https://openclassrooms.com/fr/courses/6390246-passez-au-full-stack-avec-node-js-express-et-mongodb
---
+++ models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const userSchema = mongoose.User({
+  email: { type: String, required: true, unique: true }, //unique permet de garantir qu'on utilisera un mail une seule et unique fois
+  password: { type: String, required: true },
+});
+
+userSchema.plugin(uniqueValidator); // on est sur qu'une adresse ne pourra être utilisée deux fois
+
+module.exports = mongoose.model("User", userSchema);

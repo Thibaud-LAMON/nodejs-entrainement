46af4502b4efcb74addffc0cda062ab49502cab6
Thibaud-LAMON <thib.lamon@gmail.com>
2023-11-22 15:22:45 +0100

    Créer le middleware d'authentification

---
+++ middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1]; //on récupère le header et le divise en tableau au niveau de l'espace entre le bearer[0] et le token[1]
+    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET"); //on le décode avec verify(le token, la clé secrète)
+    const userId = decodedToken.userId; // on récupère le userId
+    req.auth = {
+      userId: userId, //on rajoute userId
+    };
+    next();
+  } catch (error) {
+    //en cas d'erreur (dont token invalide)
+    res.status(401).json({ error });
+  }
+};
--- routes/stuff.js
+++ routes/stuff.js
@@ -1,12 +1,13 @@
 const express = require("express"); //on importe express
+const auth = require("../middleware/auth");
 const router = express.Router(); //on créer le routeur avec la méthode Router
 const stuffCtrl = require("../controllers/stuff"); // on importe le controller stuff
 
-//2 arguments : URL & fonction réponse appelée depuis le contrôleur
-router.post("/", stuffCtrl.createThing); //intercepte uniquement les requêtes HTTP de type POST
-router.put("/:id", stuffCtrl.modifyThing); //intercepte uniquement les requêtes HTTP de type PUT
-router.delete("/:id", stuffCtrl.deleteThing); //intercepte uniquement les requêtes HTTP de type DELETE
-router.get("/:id", stuffCtrl.getOneThing); // :id = segment dynamique, pour récupérer un objet selon son id
-router.get("/", stuffCtrl.getAllThings); //n'intercepte que les requêtes GET
+//3 arguments : URL, authentification & fonction réponse appelée depuis le contrôleur
+router.post("/", auth, stuffCtrl.createThing); //intercepte uniquement les requêtes HTTP de type POST
+router.put("/:id", auth, stuffCtrl.modifyThing); //intercepte uniquement les requêtes HTTP de type PUT
+router.delete("/:id", auth, stuffCtrl.deleteThing); //intercepte uniquement les requêtes HTTP de type DELETE
+router.get("/:id", auth, stuffCtrl.getOneThing); // :id = segment dynamique, pour récupérer un objet selon son id
+router.get("/", auth, stuffCtrl.getAllThings); //n'intercepte que les requêtes GET
 
 module.exports = router;
